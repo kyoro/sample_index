c6aefd4741ccfc7ae19224db533ac539c220e7f7
Kyosuke INOUE <kyoro@hakamastyle.net>
2011-05-27 12:29:35 -0700

    spel

--- public_html/cgi/index.pl
+++ public_html/cgi/index.pl
@@ -1,11 +1,10 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-
 use FindBin::libs;
 #use MyApp::Hello;
-
 #my $hello = MyApp::Hello->new();
 #$hello->run();
 
 print "Content-type: text/html\n\n";
+print "aaa";
