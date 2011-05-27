056d3c412d72a81fdf1e9f2b62334a1fe9fe0094
Kyosuke INOUE <kyoro@hakamastyle.net>
2011-05-27 12:25:33 -0700

    spel

--- public_html/cgi/index.pl
+++ public_html/cgi/index.pl
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 
 use FindBin::libs;
-use MyApp::Hello;
+#use MyApp::Hello;
 
-my $hello = MyApp::Hello->new();
-$hello->run();
+#my $hello = MyApp::Hello->new();
+#$hello->run();
