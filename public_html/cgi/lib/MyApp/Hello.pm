ccdbae839e2942815d3d0ef37a5e24a56620d6b0
Kyosuke INOUE <kyoro@hakamastyle.net>
2011-05-27 13:10:22 -0700

    cc

--- public_html/cgi/lib/MyApp/Hello.pm
+++ public_html/cgi/lib/MyApp/Hello.pm
@@ -4,7 +4,7 @@ use base 'CGI::Application';
 
 sub setup {
     my $self = shift;
-    $self->tmpl_path('/home/miko/public_html/cgi/tmpl');
+    $self->tmpl_path('tmpl');
     $self->start_mode('hello');
     $self->run_modes(
         'hello' => 'hello',
@@ -12,7 +12,8 @@ sub setup {
 }
 
 sub hello {
-my $html = $self->load_tmpl('index.tmpl');
+    my $self = shift;
+    my $html = $self->load_tmpl('index.tmpl');
     return $html->output;
     return "aaaa";
 }
