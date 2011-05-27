6aa24cf68c2e1587e8773afbf1f1c8604f0b399c
Kyosuke INOUE <kyoro@hakamastyle.net>
2011-05-27 01:34:55 -0700

    aa

--- public_html/perl/index.pl
+++ public_html/perl/index.pl
@@ -1,9 +1,5 @@
 #!/usr/bin/perl
-use File::Basename;
-my $w_dir;
-BEGIN { $w_dir = dirname $ENV{_} }
-use lib "$w_dir/lib";
-
+use FindBin::libs;
 use Hakamastyle::Test;
 
 print "Content-type: text/html\n\n";
