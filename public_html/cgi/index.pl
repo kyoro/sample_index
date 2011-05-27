33bc601d42711d0b73a942a74ec77b2d47e6412f
Kyosuke INOUE <kyoro@hakamastyle.net>
2011-05-27 12:39:55 -0700

    disp

--- public_html/cgi/index.pl
+++ public_html/cgi/index.pl
@@ -3,5 +3,11 @@ use strict;
 use warnings;
 use FindBin::libs;
 use MyApp::Hello;
-my $hello = MyApp::Hello->new();
-$hello->run();
+
+#my $hello = MyApp::Hello->new();
+#$hello->run();
+
+CGI::Application::Dispatch->dispatch(
+    PREFIX  => 'MyApp',
+    DEFAULT => 'Hello',
+);
